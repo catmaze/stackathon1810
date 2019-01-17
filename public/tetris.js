f8b893b5734ed6ea3dea1b4bcaee34602db2a30e
Pavel Machuca <pavelmachuca@hotmail.com>
2019-01-17 15:49:17 -0500

    drawSquare() draws squares

--- public/tetris.js
+++ public/tetris.js
@@ -1,4 +1,4 @@
-// import { I, J, L, O, S, T, Z } from './tetrominoes';
+// import { I, J, L, O, S, T, Z } from './tetrominoes.js';
 
 // draw square
 function drawSquare(x, y, color) {
@@ -6,15 +6,19 @@ function drawSquare(x, y, color) {
   const context = canvas.getContext('2d');
   const scoreElement = document.getElementById('score');
 
-  const row = 20;
-  const col = 20;
-  const squareSize = 20;
-  const vacant = 'white';
+  const ROW = 20;
+  const COL = 20;
+  // square size
+  const SQ = 20;
+  const VACANT = 'WHITE';
 
   context.fillStyle = color;
-  context.fillRect(x * squareSize, y * squareSize, squareSize, squareSize);
+  context.fillRect(x * SQ, y * SQ, SQ, SQ);
 
   context.strokeStyle = 'black';
-  context.strokeRect(x * squareSize, y * squareSize, squareSize, squareSize);
+  context.strokeRect(x * SQ, y * SQ, SQ, SQ);
 }
-drawSquare(1, 1, 'red');
+drawSquare(5, 5, 'red');
+drawSquare(5, 3, 'red');
+drawSquare(3, 3, 'red');
+drawSquare(3, 5, 'red');
