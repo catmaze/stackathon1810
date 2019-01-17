ccc9ae414f72dd15cd25f897d39ce106535a10bf
Pavel Machuca <pavelmachuca@hotmail.com>
2019-01-17 15:23:03 -0500

    trying to add code to React

--- client/components/Board.js
+++ client/components/Board.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+// import drawSquare from './tetris';
+
 const Board = () => {
   return (
     <div>
@@ -10,4 +12,21 @@ const Board = () => {
   );
 };
 
+function drawSquare(x, y, color) {
+  const canvas = document.getElementById('tetris');
+  const context = canvas.getContext('2d');
+  const scoreElement = document.getElementById('score');
+
+  const row = 20;
+  const col = 20;
+  const squareSize = 20;
+  const vacant = 'white';
+
+  context.fillStyle = color;
+  context.fillRect(x * squareSize, y * squareSize, squareSize, squareSize);
+
+  context.strokeStyle = 'black';
+  context.strokeRect(x * squareSize, y * squareSize, squareSize, squareSize);
+}
+drawSquare(1, 1, 'black');
 export default Board;
---
+++ client/components/tetris.js
@@ -0,0 +1,19 @@
+import { I, J, L, O, S, T, Z } from './tetrominoes';
+
+// draw square
+export default function drawSquare(x, y, color) {
+  const canvas = document.getElementById('tetris');
+  const context = canvas.getContext('2d');
+  const scoreElement = document.getElementById('score');
+
+  const row = 20;
+  const col = 20;
+  const squareSize = 20;
+  const vacant = 'white';
+
+  context.fillStyle = color;
+  context.fillRect(x * squareSize, y * squareSize, squareSize, squareSize);
+
+  context.strokeStyle = 'black';
+  context.strokeRect(x * squareSize, y * squareSize, squareSize, squareSize);
+}
