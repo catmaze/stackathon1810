18978e0d5cbc768e9c2d977061a5a28361654e54
pavel6767 <pavelmachuca@hotmail.com>
2019-01-21 23:10:29 -0500

    added icon

--- main.js
+++ main.js
@@ -8,7 +8,8 @@ function createWindow() {
   // Create the browser window.
   win = new BrowserWindow({
     width: 800,
-    height: 600
+    height: 600,
+    icon: `${__dirname}/public/icon.ico`
   });
 
   // and load the index.html of the app.
